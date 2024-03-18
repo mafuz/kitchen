58491c4646906643f5627ceb1daa4da59e09e0b3
mafuz <mmutarum@gmail.com>
2024-03-18 10:53:16 +0100

    Update ProductCategories.js

--- src/screens/home/ProductCategories.js
+++ src/screens/home/ProductCategories.js
@@ -9,18 +9,18 @@ import { useNavigate } from "react-router-dom";
 const categories = [
   {
     id: 1,
-    title: "Gadgets",
-    image: "https://i.ibb.co/5GVkd3m/c1.jpg",
+    title: "Wrist Watch",
+    image: "https://i.ibb.co/XYgQCQz/c1.jpg",
   },
   {
     id: 2,
-    title: "Womens Fashion",
-    image: "https://i.ibb.co/nQKLjrW/c2.jpg",
+    title: "Women Fashion",
+    image: "https://i.ibb.co/zNSrLbJ/c2.jpg",
   },
   {
     id: 3,
-    title: "Sport Sneakers",
-    image: "https://i.ibb.co/fNkBYgr/c3.jpg",
+    title: "Men Fashion",
+    image: "https://i.ibb.co/DWTsk2W/c3.jpg",
   },
 ];
 
